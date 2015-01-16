4a67db0796441fa9bdb027a93e20994af1da62c1
Josh Hunt <josh@trtr.co>
2015-01-17 01:19:58 +1100

    Updated swipe

--- swipe/script.js
+++ swipe/script.js
@@ -33,88 +33,93 @@
   };
 
   document.addEventListener('DOMContentLoaded', function() {
-    var BASE_DURATION, START_X, TRESHOLD, isTransitioning, mc, onPanEnd, row, snapTo, transformX;
-    row = document.querySelector('.swipeable');
-    mc = new Hammer(row, {});
+    var BASE_DURATION, TRESHOLD, isTransitioning, rows;
+    rows = document.querySelectorAll('.row .row__body');
     TRESHOLD = -80;
     BASE_DURATION = 600;
     isTransitioning = false;
-    START_X = 0;
-    transformX = function(value) {
-      return window.requestAnimationFrame(function() {
-        return row.style[_xformProp] = translate3d(value);
+    return [].forEach.call(rows, function(row) {
+      var POS_X, mc, onPanEnd, snapTo, transformX;
+      mc = new Hammer(row, {});
+      POS_X = 0;
+      transformX = function(value) {
+        return window.requestAnimationFrame(function() {
+          return row.style[_xformProp] = translate3d(value);
+        });
+      };
+      snapTo = function(destX) {
+        var animate, distance, duration, startTime, startX, stop, x;
+        isTransitioning = true;
+        stop = false;
+        startX = getTranslateX(row);
+        x = startX;
+        distance = Math.min(100, destX - startX);
+        duration = BASE_DURATION * Math.min(1.2, Math.max(0.6, Math.abs(distance / 768)));
+        startTime = Date.now();
+        animate = null;
+        animate = function() {
+          var now, p, timeSoFar, val;
+          if (stop) {
+            isTransitioning = false;
+            console.groupEnd();
+            POS_X = destX;
+            return;
+          }
+          window.requestAnimationFrame(animate);
+          now = Date.now();
+          timeSoFar = now - startTime;
+          console.log('timeSoFar:', timeSoFar);
+          if (timeSoFar >= duration) {
+            stop = true;
+          }
+          p = (now - startTime) / duration;
+          val = outQuart(p);
+          x = startX + (destX - startX) * val;
+          console.log('Moving to', x);
+          return row.style[_xformProp] = translate3d(x);
+        };
+        console.groupCollapsed('Animating...', {
+          startX: startX,
+          x: x,
+          destX: destX,
+          distance: distance,
+          duration: duration
+        });
+        return animate();
+      };
+      window.snapTo = snapTo;
+      onPanEnd = function(ev) {
+        var move;
+        POS_X = getTranslateX(row);
+        if (POS_X > TRESHOLD / 2) {
+          move = 0;
+        } else {
+          move = TRESHOLD;
+        }
+        return snapTo(move);
+      };
+      mc.on('panend', onPanEnd);
+      mc.on('pancancel', onPanEnd);
+      mc.on('tap', function() {
+        if (POS_X !== 0) {
+          return snapTo(0);
+        }
       });
-    };
-    snapTo = function(destX) {
-      var animate, distance, duration, startTime, startX, stop, x;
-      isTransitioning = true;
-      stop = false;
-      startX = getTranslateX(row);
-      x = startX;
-      distance = Math.min(100, destX - startX);
-      duration = BASE_DURATION * Math.min(1.2, Math.max(0.6, Math.abs(distance / 768)));
-      startTime = Date.now();
-      animate = null;
-      animate = function() {
-        var now, p, timeSoFar, val;
-        if (stop) {
-          isTransitioning = false;
-          console.groupEnd();
-          START_X = destX;
+      return mc.on('panleft panright', function(ev) {
+        var move;
+        if (isTransitioning) {
           return;
         }
-        window.requestAnimationFrame(animate);
-        now = Date.now();
-        timeSoFar = now - startTime;
-        console.log('timeSoFar:', timeSoFar);
-        if (timeSoFar >= duration) {
-          stop = true;
+        if (ev.deltaX > 0) {
+          snapTo(0);
+          return;
         }
-        p = (now - startTime) / duration;
-        val = outQuart(p);
-        x = startX + (destX - startX) * val;
-        console.log('Moving to', x);
-        return row.style[_xformProp] = translate3d(x);
-      };
-      console.groupCollapsed('Animating...', {
-        startX: startX,
-        x: x,
-        destX: destX,
-        distance: distance,
-        duration: duration
+        move = POS_X + ev.deltaX;
+        if (move > 0) {
+          return;
+        }
+        return transformX(move);
       });
-      return animate();
-    };
-    window.snapTo = snapTo;
-    onPanEnd = function(ev) {
-      var move;
-      START_X = getTranslateX(row);
-      if (START_X > TRESHOLD / 2) {
-        move = 0;
-      } else {
-        move = TRESHOLD;
-      }
-      return snapTo(move);
-    };
-    mc.on('panend', onPanEnd);
-    mc.on('pancancel', onPanEnd);
-    mc.on('tap', function() {
-      return snapTo(0);
-    });
-    return mc.on('panleft panright', function(ev) {
-      var move;
-      if (isTransitioning) {
-        return;
-      }
-      if (ev.deltaX > 0) {
-        snapTo(0);
-        return;
-      }
-      move = START_X + ev.deltaX;
-      if (move > 0) {
-        return;
-      }
-      return transformX(move);
     });
   });
 
